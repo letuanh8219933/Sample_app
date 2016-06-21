a1f5981bce903cdf55d03f98f9aff16710e9973e
Le Tu Anh <le.tu.anh@framgia.com>
2016-06-21 15:18:46 +0700

    finish user edit, update,index,and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
   # Returns the current logged-in user (if any).
   def current_user
        if (user_id = session[:user_id])
@@ -42,12 +46,24 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      raise       # The tests still pass, so this branch is currently untested.
+      raise       
+      # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
