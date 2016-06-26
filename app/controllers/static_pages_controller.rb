07d0d57fd2b9cf5e38c1495112dcd60c7c839abb
Le Tu Anh <letuanh8219932@gmail.com>
2016-06-26 11:32:04 +0700

    chapter11

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
-  def About
+
+  def home
+    @micropost = current_user.microposts.build if logged_in?
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
-  def Contact
+  def help
   end
 
-  def News
+  def about
+  end
+
+  def contact
   end
 end
